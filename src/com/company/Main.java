ed4c8c24ac5aa26753e2a858a9a90d5e6f7e1b78
CrazyFish2262 <vlad.bogomoltsev_gh>
2021-11-08 20:11:42 +0300

    Внесены изменения, добавлены строчки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.printf("My new branch");
+        System.out.printf("Keep going");
 
-
-        //внесены некоторые изменения
     }
 }
