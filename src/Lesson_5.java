aaee5e87cb784a3213c5c49416f06df656bfdb6c
CrazyFish2262 <vlad.bogomoltsev_gh>
2021-11-08 20:46:28 +0300

    Задания на уроке

---
+++ src/Lesson_5.java
@@ -0,0 +1,17 @@
+public class Lesson_5 {
+    public static void main(String[] args) {
+
+        sum(5, 6);
+        sum(565, 6);
+        sum(5, 679);
+        sum(5, 45);
+        sum(345, 6);
+        sum(5, 66);
+
+    }
+
+    private static void sum(int a, int b) {
+        System.out.println(a + b);
+        return;
+    }
+}
