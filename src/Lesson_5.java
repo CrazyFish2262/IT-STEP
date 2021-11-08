b00abe9230eac180050b3abfe966c0aa0da2b941
Владислав <!>
2021-11-08 23:49:36 +0300

    Исправил последнее задание

---
+++ src/Lesson_5.java
@@ -0,0 +1,65 @@
+public class Lesson_5 {
+    public static void main(String[] args) {
+
+/*      sum(451, 2, 4);
+        subtract(5, 6);
+        multiple(8, 7);
+        div(8, 2);
+        strings("Hello", "Vasya");
+        int a = array(1, 7, 8, 9);
+        System.out.println(a);
+    }
+
+    private static void sum(int a, int b, int c) {
+        System.out.println(a + b + c);
+        return;
+
+    }
+
+    private static void subtract(int a, int b) {
+        System.out.println(a - b);
+        return;
+    }
+
+    private static void multiple(int a, int b) {
+        System.out.println(a * b);
+        return;
+    }
+
+    private static void div(int a, int b) {
+        System.out.println(a / b);
+        return;
+    }
+
+    private static void strings(String a, String b) {
+        System.out.println(a + " " + b);
+        return;
+    }
+
+    private static int array(int... a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+
+    }
+    */
+
+
+  qwerty(34,546,235,"hello, world");
+
+}
+
+
+    private static void qwerty( int c, double d, long e, String f) {
+        System.out.print("Int is " + c + " and " + "Double is " + d + " and " + "String is " + "\"" +f + "\"");
+        return;
+    }
+
+
+    }
+
+
+
+
