9f953eaa18b5cf3873fd2adecbdcb90d826a4012
CrazyFish2262 <vlad.bogomoltsev_gh>
2021-11-08 21:49:16 +0300

    Задания на уроке, нужно посмотреть и исправить

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -1,17 +1,65 @@
 public class Lesson_5 {
     public static void main(String[] args) {
 
-        sum(5, 6);
-        sum(565, 6);
-        sum(5, 679);
-        sum(5, 45);
-        sum(345, 6);
-        sum(5, 66);
+/*      sum(451, 2, 4);
+        subtract(5, 6);
+        multiple(8, 7);
+        div(8, 2);
+        strings("Hello", "Vasya");
+        int a = array(1, 7, 8, 9);
+        System.out.println(a);
+    }
+
+    private static void sum(int a, int b, int c) {
+        System.out.println(a + b + c);
+        return;
 
     }
 
-    private static void sum(int a, int b) {
-        System.out.println(a + b);
+    private static void subtract(int a, int b) {
+        System.out.println(a - b);
         return;
     }
+
+    private static void multiple(int a, int b) {
+        System.out.println(a * b);
+        return;
+    }
+
+    private static void div(int a, int b) {
+        System.out.println(a / b);
+        return;
+    }
+
+    private static void strings(String a, String b) {
+        System.out.println(a + " " + b);
+        return;
+    }
+
+    private static int array(int... a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+
+    }
+    */
+
+
+  qwerty(34,546,235,55,76743, "hello, world");
+
 }
+
+
+    private static void qwerty(byte a, short b, int c, double d, long e, String f) {
+        System.out.print("Byte is " + a + " and " + "Short is " + b + " and " + "Int is " + c + " and " + "Double is " + d);
+        return;
+    }
+
+
+    }
+
+
+
+
